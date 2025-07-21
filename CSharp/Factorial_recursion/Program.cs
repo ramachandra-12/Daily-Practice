0c53bdbc4bbae8603f6cdd3a74a335c83761527b
Ramachandra <ramachandrabammidi12@gmail.com>
2025-07-21 23:06:13 +0530

    Factorial

---
+++ CSharp/Factorial_recursion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        int N=int.Parse(Console.ReadLine()!);
+        long result=Fact(N);
+        Console.WriteLine(result);
+    }
+    static long Fact(int N)
+    {
+        if (N==0)
+        {
+            return 1;
+        }
+        else
+        {
+            return N*Fact(N-1);
+        }
+        
+    }
+}
