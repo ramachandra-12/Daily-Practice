3f6ced1264800877fbda59a003d8f241b31b854b
Ramachandra <ramachandrabammidi12@gmail.com>
2025-07-23 21:47:42 +0530

    LCM_HCF

---
+++ LCM_HCF/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) 
+    {
+        int T=int.Parse(Console.ReadLine());
+        for (int t=0;t<T;t++)
+        {
+            string[] tokens=Console.ReadLine().Split();
+            int A=int.Parse(tokens[0]);
+            int B=int.Parse(tokens[1]);
+            int HCF=gcd(A,B);
+            long LCM=((long)A*B)/HCF;
+            Console.WriteLine($"{LCM} {HCF}");
+            
+        }
+
+    }
+    static int gcd(int a,int b)
+    {
+        if (b==0)
+        {
+            return a;
+        }
+        else{
+            return gcd(b,a%b);
+        }
+
+    } 
+}
