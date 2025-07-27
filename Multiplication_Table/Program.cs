16855539ff0e1676d5773cad2c2dc228c9685e88
Ramachandra <ramachandrabammidi12@gmail.com>
2025-07-27 22:35:24 +0530

    Multiplication Table

---
+++ Multiplication_Table/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        int N=int.Parse(Console.ReadLine());
+        for(int i=1;i<=10;i++)
+        {
+            int prod=N*i;
+            Console.WriteLine($"{N} * {i} = {prod}");
+        }
+    }
+}
