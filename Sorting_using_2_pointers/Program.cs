48cb46deaa2e0416541039c808ec7db52e012737
Ramachandra <ramachandrabammidi12@gmail.com>
2025-08-10 17:13:59 +0530

    Sorting using two pointers

---
+++ Sorting_using_2_pointers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        int T=int.Parse(Console.ReadLine());
+        for(int t=0;t<T;t++)
+        {
+            int N=int.Parse(Console.ReadLine());
+            int[] array=Array.ConvertAll(Console.ReadLine().Split(),int.Parse);
+            int left=0,right=N-1;
+            while(left<right)
+            {
+                if(array[left]==0)
+                {
+                    left++;
+                }
+                else if(array[right]==1)
+                {
+                    right--;
+                }
+                else{
+                    int temp=array[left];
+                    array[left]=array[right];
+                    array[right]=temp;
+                }
+            }
+            Console.WriteLine(String.Join(" ",array));
+        }
+    }
+}
---
+++ Sorting_using_2_pointers/README.md
@@ -0,0 +1,27 @@
+#Sort 0s and 1s 
+
+You are given an array of 0's and 1's. Sort the array in ascending order and print it.
+
+Note: 
+ Solve using two-pointer technique.  Input Format
+The first line of input contains T - the number of test cases. It's followed by 2T lines, the first line contains N - the size of the array. The second line contains the elements of the array.
+
+Output Format
+For each test case, sort the array in ascending order and print it in a new line.
+
+Constraints
+1 <= T <= 1000
+1 <= N <= 1000
+0 <= A[i] <= 1
+
+Example
+Input
+2
+5
+0 1 1 0 1
+6
+1 1 1 1 1 0
+
+Output
+0 0 1 1 1
+0 1 1 1 1 1
