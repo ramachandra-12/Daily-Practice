6519b455f07618c743863e7a653bf2a3d740ff15
Ramachandra <ramachandrabammidi12@gmail.com>
2025-08-30 17:46:13 +0530

    Checkerboard Pattern problem

---
+++ Checkerboard_Pattern/Checkerboard Pattern.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution {
+    static void Main(String[] args) {
+        int T=int.Parse(Console.ReadLine());
+        for (int t=1;t<=T;t++)
+        {
+            int N=int.Parse(Console.ReadLine());
+            int size=2*N;
+            Console.WriteLine($"Case #{t}:");
+            for(int i=0;i<size;i++)
+            {
+                for(int j=0;j<size;j++)
+                {
+                    int row=i/2;
+                    int col=j/2;
+                    if((row+col)%2==0)
+                    {
+                        Console.Write("*");
+                    }
+                    else{
+                        Console.Write("-");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+            
+        }
+    }
+}
---
+++ Checkerboard_Pattern/README.md
@@ -0,0 +1,34 @@
+#Checkerboard Pattern 
+
+You are given an integer N. Print a 2N×2N checkerboard consisting of 2×2 squares alternating '*' and '-', with the top-left cell being '*'. See the following examples for N = 1,2,3 and 4 respectively.
+
+Input Format
+The first line of input contains T - the number of test cases. It is followed by T lines, each line contains a single integer N - the size of the pattern.
+
+Output Format
+For each test case, print the test case number as shown, followed by the pattern, separated by a new line.
+
+Constraints
+1 <= T <=100
+1 <= N <=100
+
+Example
+Input
+2
+4
+2
+
+OutputCase #1:
+**--**--
+**--**--
+--**--**
+--**--**
+**--**--
+**--**--
+--**--**
+--**--**
+Case #2:
+**--
+**--
+--**
+--**
